1c2570daa33fa3cee62ad13869d6162ad8b25bbe
fayazshaik515 <fayazshaik231104@gmail.com>
2024-07-13 11:04:45 +0530

    javapractice

---
+++ object1.java
@@ -0,0 +1,8 @@
+public class object1{
+    public void fayaz(){
+        System.out.println("from eluru");
+        System.out.println("completed btech");
+        System.out.println("age 21");
+    }
+
+    }
---
+++ object2.java
@@ -0,0 +1,7 @@
+public class object2 {
+    public static void main(String[] args){
+        object1 details = new object1();
+        details.fayaz();
+    }
+    
+}

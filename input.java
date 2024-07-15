ff1945c30aa4b56bbe14b2e453573260659db71c
fayazshaik515 <fayazshaik231104@gmail.com>
2024-07-15 12:04:18 +0530

    huh

--- input.java
+++ input.java
@@ -1,10 +1,9 @@
 import java.util.Scanner;
 public class input {
     public static void main (String[] args){
-        String f;
         System.out.println("enter your name");
         Scanner fa = new Scanner(System.in);
-        f = fa.nextLine();
+        String f = fa.nextLine();
         System.out.println("your name is " +f);
     }
 

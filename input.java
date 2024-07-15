57289fbe5140df47b9b1b24353b4703828f93dfa
fayazshaik515 <fayazshaik231104@gmail.com>
2024-07-15 11:58:13 +0530

    commit

--- input.java
+++ input.java
@@ -1,12 +1,11 @@
 import java.util.Scanner;
 public class input {
     public static void main (String[] args){
-        String  f ;
+        String f;
         System.out.println("enter your name");
         Scanner fa = new Scanner(System.in);
-        f = fa.next();
+        f = fa.nextLine();
         System.out.println("your name is " +f);
-
     }
 
     
